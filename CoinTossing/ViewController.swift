279e1702dc899dce936c20dca79ca6e7a7c2048f
tttdn <118190673+tttdn@users.noreply.github.com>
2021-08-24 14:50:54 +0000

    improving tests

---
+++ CoinTossing/ViewController.swift
@@ -0,0 +1,12 @@
+
+//
+//  ViewController.swift
+//  CoinTossing
+//
+//  Created by Shashikant Jagtap on 13/01/2017.
+//  Copyright © 2017 Shashikant Jagtap. All rights reserved.
+//
+
+import UIKit
+
+extension Array {
