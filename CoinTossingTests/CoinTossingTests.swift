b836d7f03041d0bf3e170f43597f4543e1963b0b
tttdn <118190673+tttdn@users.noreply.github.com>
2022-06-06 18:38:31 +0000

    Remove unused usings.

--- CoinTossingTests/CoinTossingTests.swift
+++ CoinTossingTests/CoinTossingTests.swift
@@ -3,4 +3,8 @@
 //  CoinTossingTests
 //
 //  Created by Shashikant Jagtap on 13/01/2017.
-//  Copyright © 2017 Shashikant Jagt
+//  Copyright © 2017 Shashikant Jagtap. All rights reserved.
+//
+
+import XCTest
+@testable import Coi
