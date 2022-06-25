43866563a9bab64dbea1611b92c6301de10c9da0
tttdn <118190673+tttdn@users.noreply.github.com>
2022-06-25 17:11:31 +0000

    ToC

--- CoinTossingTests/CoinTossingTests.swift
+++ CoinTossingTests/CoinTossingTests.swift
@@ -30,4 +30,7 @@ class CoinTossingTests: XCTestCase {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
-       
+        }
+    }
+    
+}
